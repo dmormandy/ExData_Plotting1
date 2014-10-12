49d484b8a22e41d0e2b154d7b5cf0177e5f43b0b
Danielle Ormandy <danielle.d.ormandy@ericsson.com>
2014-10-12 21:06:01 +1100

    plot1.R

---
+++ plot1.R
@@ -0,0 +1,14 @@
+# Read Data from provided file
+mydata <- read.csv("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
+# Change class of Date and Time
+mydata[[1]] <- as.Date(mydata[[1]], format="%d/%m/%Y")
+mydata[[2]] <- strptime(mydata[[2]],format="%H:%M", tz="")
+# Subset on provided Date range
+testset <- subset(mydata, Date >="2007-02-01" & Date <="2007-02-02")
+# Take Global active power data for plot 1 as x
+x <- as.numeric(testset["Global_active_power"])
+# Start plot
+png('plot1.png',width=480,height=480)
+hist(x$Global_active_power,col="red",main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+
