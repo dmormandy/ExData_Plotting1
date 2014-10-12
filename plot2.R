2d969b14e324a1cdd8ac0df3ec905b73361bb15b
Danielle Ormandy <danielle.d.ormandy@ericsson.com>
2014-10-12 21:46:02 +1100

    plot2.R

---
+++ plot2.R
@@ -0,0 +1,16 @@
+# Read Data from provided file
+mydata <- read.csv("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
+# Change class of Date and Time
+mydata[[1]] <- as.Date(mydata[[1]], format="%d/%m/%Y")
+# Subset on provided Date range
+testset <- subset(mydata, Date >="2007-02-01" & Date <="2007-02-02")
+# Combine Date and Time
+dt <- paste(as.character(testset$Date), testset$Time)
+#Reformat with Day of the Week
+dt <- strptime(dt, format="%Y-%m-%d %H:%M:%S")
+dw <- strftime(dt, format="%A %Y-%m-%d %H:%M:%S", tz="")
+# Start plot
+png('plot2.png',width=480,height=480)
+plot(dt,testset$Global_active_power, type="n",ylab="Global Active Power (kilowatts)",xlab="")
+lines(dt,testset$Global_active_power,type="l")
+dev.off()
